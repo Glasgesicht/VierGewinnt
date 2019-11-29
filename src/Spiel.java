826fade489fcbb77de148dd6b8f0706f30669b72
Poerling <44144795+Poerling@users.noreply.github.com>
2019-11-29 10:43:42 +0100

    Update Spiel.java

--- src/Spiel.java
+++ src/Spiel.java
@@ -2,7 +2,7 @@
 public class Spiel implements FourWinsLogic{
 
     Spieler Spielfeld[][] = new Spieler[6][7];
-    
+    //hallo
     @Override
     public Ergebnis throwChrip(Spieler p, int coll) {
         // TODO Auto-generated method stub
